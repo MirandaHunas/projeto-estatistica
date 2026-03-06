bd75783e0acf63cc021590e9387a2ccc8583730b
GUILHERME RIBEIRO ROCCO <47199630808@info.local>
2026-03-06 20:20:41 -0300

    Atualizado com função limpar_dados

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # Projeto Colaborativo - Ciencia de Dados
 dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
-def limpar_dados () :
-# Retorne uma lista apenas com int ou float
-    pass
+
+def limpar_dados(lista):
+    dados_limpos = []
+    
+    for item in lista:
+        if isinstance(item, (int, float)):
+            dados_limpos.append(item)
+            
+    return dados_limpos
+
+resultado = limpar_dados(dados_sujos)
+print(resultado)
 def calcular_media () :
     pass
 def calcular_mediana () :
@@ -13,3 +22,5 @@ def obter_extremos () :
     pass
 dados = limpar_dados ( dados_sujos )
 print ( f" Dados processados : { dados }")
+
+print("Verificado por: Guilherme Rocco")
